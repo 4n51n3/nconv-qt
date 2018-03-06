67abb768413537a1dfed0eec514fb3bf9e0bee4b
Unknown <kotteam99@gmail.com>
2018-03-06 23:16:49 +0200

    fixes

--- Converter.h
+++ Converter.h
@@ -6,100 +6,109 @@
 
 using namespace std;
 
-class ConvertException : public exception{
+
+class ConvertException : public exception {
 public:
-virtual const char* what() = 0;
+    virtual const char *what() = 0;
 };
 
-class NotAllowedSymbolException : ConvertException{
+class NotAllowedSymbolException : public ConvertException {
 public:
     string msg = "[EXCEPTION] Please enter only allowed symbols.";
-    const char* what(){
-    	return msg.c_str();
+
+    const char *what() {
+        return msg.c_str();
     }
 };
 
-class BaseOutOfRangeException{
+class BaseOutOfRangeException : public ConvertException {
 public:
     string msg = "[EXCEPTION] Base must be smaller than 37 and bigger than 1.";
+
+    const char *what() {
+        return msg.c_str();
+    }
 };
 
-class DigitGreaterThanBaseException{
+class DigitGreaterThanBaseException : public ConvertException {
 public:
     string msg = "[EXCEPTION] Digits may be smaller than base.";
+
+    const char *what() {
+        return msg.c_str();
+    }
 };
 
-class Converter{
+class Converter {
 private:
-typedef unsigned long ulong;
-typedef unsigned int uint;
+    typedef unsigned long ulong;
+    typedef unsigned int uint;
 
 
-map<int, char> intChar;
-map<char, int> charInt;
+    map<int, char> intChar;
+    map<char, int> charInt;
 
 
-
-
-void toUpper(string &str) {
-    for (auto &c:str) {
-        c = (char) toupper(c);
-    }
-}
-
-bool IsAlphaNum(string number) {
-    ulong size = number.size();
-    for (int i = 0; i < size; i++) {
-        if (!isalnum(number[i])) {
-            return false;
+    void toUpper(string &str) {
+        for (auto &c:str) {
+            c = (char) toupper(c);
         }
     }
-    return true;
-}
+
+    bool IsAlphaNum(string number) {
+        ulong size = number.size();
+        for (int i = 0; i < size; i++) {
+            if (!isalnum(number[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 public:
 
-Converter(){
- for (int i = 0; i < 26; i++) {
-        intChar.insert(pair<int, char>(10 + i, (char) ('A' + i)));
-        charInt.insert(pair<char, int>((char) ('A' + i), 10 + i));
-    }
-    for (int i = 0; i < 10; i++) {
-        intChar.insert(pair<int, char>(i, (char) ('0' + i)));
-        charInt.insert(pair<char, int>((char) ('0' + i), i));
-    }
-}
-uint GetDecNumber(string str, uint base) {
-    if (!IsAlphaNum(str))
-        throw NotAllowedSymbolException();
-    uint dec = 0;
-    reverse(str.begin(), str.end());
-    for (int i = 0; i < str.size(); i++) {
-        if (charInt[str[i]] >= base) {
-            throw DigitGreaterThanBaseException();
+    Converter() {
+        for (int i = 0; i < 26; i++) {
+            intChar.insert(pair<int, char>(10 + i, (char) ('A' + i)));
+            charInt.insert(pair<char, int>((char) ('A' + i), 10 + i));
         }
-        dec += charInt[str[i]] * pow(base, i);
+        for (int i = 0; i < 10; i++) {
+            intChar.insert(pair<int, char>(i, (char) ('0' + i)));
+            charInt.insert(pair<char, int>((char) ('0' + i), i));
+        }
     }
-    return dec;
-}
 
-string Convert(const uint decNumber, const uint base) {
-    uint div = decNumber;
-    string str;
-
-    while (div > 0) {
-        string append;
-        int remainder = div % base;
-        if (remainder >= 10)
-            append = intChar[remainder];
-        else
-            append = to_string(remainder);
-        str.append(append);
-        div /= base;
+    uint GetDecNumber(string str, uint base) {
+        if (!IsAlphaNum(str))
+            throw NotAllowedSymbolException();
+        uint dec = 0;
+        reverse(str.begin(), str.end());
+        for (int i = 0; i < str.size(); i++) {
+            if (charInt[str[i]] >= base) {
+                throw DigitGreaterThanBaseException();
+            }
+            dec += charInt[str[i]] * pow(base, i);
+        }
+        return dec;
     }
-    reverse(str.begin(), str.end());
-    return str;
-}
+
+    string Convert(const uint decNumber, const uint base) {
+        uint div = decNumber;
+        string str;
+
+        while (div > 0) {
+            string append;
+            int remainder = div % base;
+            if (remainder >= 10)
+                append = intChar[remainder];
+            else
+                append = to_string(remainder);
+            str.append(append);
+            div /= base;
+        }
+        reverse(str.begin(), str.end());
+        return str;
+    }
 
 };
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,26 +1,26 @@
 #include "mainwindow.h"
 #include <iostream>
 #include <Converter.h>
+
 using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
-    : QWidget(parent)
-{
+        : QWidget(parent) {
     ui.setupUi(this);
-    connect( ui.convertButton, SIGNAL(released()),this, SLOT(on_convertButton_clicked()));
+  //  connect(ui.convertButton, SIGNAL(released()), this, SLOT(on_convertButton_clicked()));
 }
 
-void MainWindow::on_convertButton_clicked(){
-	Converter conv;
-	uint base = stoi(ui.spinBox->text().toStdString());
-	uint toBase = stoi(ui.spinBox_2->text().toStdString());
-	string rez;
-	try{
-		rez = conv.Convert(conv.GetDecNumber(ui.lineEdit->text().toStdString(), base), toBase);
-	}catch(ConvertException &ex){
-		cout << ex.what() << endl;
-	}
-	
+void MainWindow::on_convertButton_clicked() {
+    Converter conv;
+    uint base = stoi(ui.spinBox->text().toStdString());
+    uint toBase = stoi(ui.spinBox_2->text().toStdString());
+    string rez;
+    try {
+        rez = conv.Convert(conv.GetDecNumber(ui.lineEdit->text().toStdString(), base), toBase);
+    } catch (ConvertException &ex) {
+        cout << ex.what() << endl;
+    }
+
 //	cout << "Base: " << base << "\ntoBase: " << toBase << "\nrez: "<< conv.GetDecNumber(string("523"), base) <<endl;
-	ui.lineEdit_3->setText(QString(rez.c_str()));
+    ui.lineEdit_3->setText(QString(rez.c_str()));
 }
